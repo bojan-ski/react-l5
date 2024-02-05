b5a00fdf2ebd29ac7df41af7124a98d28a164cbf
Bojan SKI <codeucenje@gmail.com>
2024-02-05 09:28:46 +0100

    code refactor and data (videos) moved to videos.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import Videos from "./components/Videos"
 
-const videos = [
-  {
-    id: 1,
-    title: 'Kurs HTML, CSS, Bootstrap',
-    url: 'https://www.youtube.com/watch?v=AAGgaOCYx_Q&ab_channel=ItMentorstva',
-    cover: 'https://i.ytimg.com/vi/AAGgaOCYx_Q/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDmWIayp0p4uYN9EqLVTnshiWKRew'
-  },
-  {
-    id: 2,
-    title: 'PHP & MYSQL kompletan kurs programiranja',
-    url: 'https://www.youtube.com/watch?v=U7jEG-N8VFw&ab_channel=ItMentorstva',
-    cover: 'https://i.ytimg.com/vi/U7jEG-N8VFw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC51n9r8jeOT2UcatTOCk8ocFkguQ'
-  },
-]
-
 function App() {
   return (
-    <>
-      {videos.map(video => {
-        return <Videos video={video}/>
-      })}
-    </>
+    <main>
+       <Videos />
+    </main>
   )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,16 @@
+import videos from '../data.json'
+
 function Videos({video}) {
     return (
-        <div className="video-data">
-            <a href={video.url} target="_blank">
-                <img src={video.cover} alt={video.title} />
-                <h3>{video.title}</h3>
-            </a>
+        <div className="videos">
+           {videos.map(video => {
+             return <div className='videos-data'>
+                <a href={video.url} target="_blank">
+                    <img src={video.cover} alt={video.title} />
+                    <h3>{video.title}</h3>
+                </a>
+             </div>             
+           })}
         </div>
     )
 }
