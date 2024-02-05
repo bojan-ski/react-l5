a9e1f1a07e0a3f98ba81790faddd89cb8587b2c7
Bojan SKI <codeucenje@gmail.com>
2024-02-05 15:41:40 +0100

    started working on the Search bar logic

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,32 @@
+import videos from '../data.json'
+
 function SearchBar() {
   return (
     <form>
         <label htmlFor="search-bar">Search by title: </label>
-        <input type="text" id="search-bar" name="search-bar"/>
+        <input type="text" id="search-bar" name="search-bar" onInput={(e) => SearchVideoByTitle(e.currentTarget.value)}/>
     </form>
   )
+}
+
+// kurs html, css, bootstrap
+
+const SearchVideoByTitle = (title) => {
+  // console.log(title);
+  videos.forEach(video => {
+    const videoTitle = video.title.toLowerCase()
+    const inputtedTitle = title.toLowerCase()
+
+    // if(videoTitle === inputtedTitle){
+    //   console.log('works');
+    // }
+
+    if(videoTitle.includes(inputtedTitle)){
+      console.log('works');
+    }else{
+      console.log('nope');
+    }
+  })
 }
 
 export default SearchBar
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,8 +3,8 @@ import videos from '../data.json'
 function Videos() {
     return (
         <section className="videos">
-           {videos.map(video => {
-             return <div className='videos-data'>
+           {videos.map((video, idx) => {
+             return <div className='videos-data' key={idx}>
                 <a href={video.url} target="_blank">
                     <img src={video.cover} alt={video.title} />
                     <h3>{video.title}</h3>
